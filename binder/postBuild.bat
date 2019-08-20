2eb97491998dd1ce936b37b1751ff57407641b3c
llorracc <llorracc@users.noreply.github.com>
2019-08-20 16:31:17 -0400

    Update binder to allow toc

--- binder/postBuild.bat
+++ binder/postBuild.bat
@@ -1,11 +1,16 @@
+@echo off
+rem = """
 jupyter contrib nbextension install --user
 jupyter nbextension enable codefolding/main
 jupyter nbextension enable codefolding/edit
-jupyter nbextension enable latex_envs
+jupyter nbextension enable toc2/main
+jupyter nbextension enable collapsible_headings/main
 python -m cite2c.install
-result=$(python <<EOF
+python -x "%~f0" %*
+exit /b %errorlevel%
+:: End of batch file commands
+"""
+
 from notebook.services.config.manager import ConfigManager
 cm = ConfigManager()
 cm.update('cite2c', {'zotero':{"user_id": "5043554","username": "econ-ark","access_token": "XZpH9NsoAZmDMmjLKiy8xMXX"}})
-EOF
-)
