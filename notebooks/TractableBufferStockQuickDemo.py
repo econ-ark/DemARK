5b0d7428ebc85b8033de6124cf1bd35b9f1af2b9
llorracc <@users.noreply.github.com>
2020-01-29 01:22:03 +0100

    Minor edits before 2nd Oslo class

--- notebooks/TractableBufferStockQuickDemo.py
+++ notebooks/TractableBufferStockQuickDemo.py
@@ -7,7 +7,7 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.2'
-#       jupytext_version: 1.2.3
+#       jupytext_version: 1.2.1
 #   kernel_info:
 #     name: python3
 #   kernelspec:
@@ -18,6 +18,9 @@
 
 # %% [markdown]
 # # The Tractable Buffer Stock Model
+
+# %% [markdown]
+# The [TractableBufferStock](http://www.econ2.jhu.edu/people/ccarroll/public/LectureNotes/Consumption/TractableBufferStock/) model is a (relatively) simple framework that captures all of the qualitative, and many of the quantitative features of optimal consumption in the presence of labor income uncertainty.  
 
 # %% {"code_folding": [0]}
 # This cell has just a bit of initial setup. You can click the arrow to expand it.
@@ -37,8 +40,6 @@ from HARK.utilities import plotFuncs
 from HARK.ConsumptionSaving.TractableBufferStockModel import TractableConsumerType
 
 # %% [markdown]
-# The [TractableBufferStock](http://www.econ2.jhu.edu/people/ccarroll/public/LectureNotes/Consumption/TractableBufferStock/) model is a (relatively) simple framework that captures all of the qualitative, and many of the quantitative features of optimal consumption in the presence of labor income uncertainty.  
-#
 # The key assumption behind the model's tractability is that there is only a single, stark form of uncertainty.  So long as an employed consumer remains employed, his labor income will rise at a constant rate.  But between any period and the next there is constant hazard $p$ of transitioning to the "unemployed" state. Unemployment is irreversible, like retirement or disability.  When unemployed, the consumer receives a fixed amount of income (for simplicity, zero).
 #
 # \begin{eqnarray*}
@@ -55,11 +56,11 @@ from HARK.ConsumptionSaving.TractableBufferStockModel import TractableConsumerTy
 #
 # where $\mathbb{1}$ is an indicator of whether the consumer is employed in the next period.
 #
-# Under plausible parameter values the model has a target level of $m = M/P$ (market resources to permanent income) with an analytical solution that exhibits plausible relationships among all of the parameters.  (See the linked handout for details).
+# Under plausible parameter values the model has a target level of $\check{m} = M/P$ (market resources to permanent income) with an analytical solution that exhibits plausible relationships among all of the parameters.  (See the [linked handout](http://www.econ2.jhu.edu/people/ccarroll/public/LectureNotes/Consumption/TractableBufferStock/) for details).
 
 # %% {"code_folding": [0]}
 # Define a parameter dictionary and representation of the agents for the tractable buffer stock model
-TBS_dictionary =  {'UnempPrb' : .00625,    # Probability of becoming unemployed
+TBS_dictionary =  {'UnempPrb' : .00625,    # Prob of becoming unemployed; working life of 1/UnempProb = 160 qtrs
                    'DiscFac' : 0.975,      # Intertemporal discount factor
                    'Rfree' : 1.01,         # Risk-free interest factor on assets
                    'PermGroFac' : 1.0025,  # Permanent income growth factor (uncompensated)
@@ -74,6 +75,7 @@ MyTBStype = TractableConsumerType(**TBS_dictionary)
 def makeTBSplot(DiscFac,CRRA,Rfree,PermGroFac,UnempPrb,mMax,mMin,cMin,cMax,plot_emp,plot_ret,plot_mSS,show_targ):
     MyTBStype.DiscFac = DiscFac
     MyTBStype.CRRA = CRRA
+    MyTBStype.Rfree = Rfree
     MyTBStype.PermGroFac = PermGroFac
     MyTBStype.UnempPrb = UnempPrb
     
@@ -131,10 +133,21 @@ CRRA_widget = widgets.FloatSlider(
     readout_format='.2f',
     description='$\\rho$')
 
+# Define a slider for the interest factor
+Rfree_widget = widgets.FloatSlider(
+    min=1.00,
+    max=1.04,
+    step=0.0001,
+    value=1.01,
+    continuous_update=False,
+    readout_format='.4f',
+    description='$R$')
+
+
 # Define a slider for permanent income growth
 PermGroFac_widget = widgets.FloatSlider(
-    min=0.9,
-    max=1.1,
+    min=0.98,
+    max=1.02,
     step=0.0002,
     value=1.0025,
     continuous_update=False,
@@ -150,16 +163,6 @@ UnempPrb_widget = widgets.FloatSlider(
     continuous_update=False,
     readout_format='.5f',
     description='$\\mho$')
-
-# Define a slider for unemployment (or retirement) probability
-Rfree_widget = widgets.FloatSlider(
-    min=1.0,
-    max=1.1,
-    step=0.0001,
-    value=1.01,
-    continuous_update=False,
-    readout_format='.4f',
-    description='$R$')
 
 # Define a text box for the lower bound of M_t
 mMin_widget = widgets.FloatText(
@@ -210,21 +213,29 @@ plot_mSS_widget = widgets.Checkbox(
 # Define a check box for whether to show the target annotation
 show_targ_widget = widgets.Checkbox(
     value=True,
-    description = 'Show target $(M,C)$',
+    description = 'Show target $(m,c)$',
     disabled = False)
 
 
 # %% [markdown]
 # ## Target Wealth
 #
-# Whether the model exhibits a "target" or "stable" level of wealth for employed consumers depends on whether the 'Growth Impatience Condition' (the GIC) holds:
+# Whether the model exhibits a "target" or "stable" level of the wealth-to-permanent-income ratio for employed consumers depends on whether the 'Growth Impatience Condition' (the GIC) holds:
 #
-# \begin{equation}\label{eq:GIC}
-#  \left(\frac{(R \beta (1-\mho))^{1/\rho}}{\Gamma}\right)  <  1
-# \end{equation}
+# \begin{align}\label{eq:GIC}
+#  \left(\frac{(R \beta (1-\mho))^{1/\rho}}{\Gamma}\right)  & <  1
+# \\ \left(\frac{(R \beta (1-\mho))^{1/\rho}}{G (1-\mho)}\right)  &<  1
+# \\ \left(\frac{(R \beta)^{1/\rho}}{G} (1-\mho)^{-\rho}\right)  &<  1
+# \end{align}
+# and recall (from [PerfForesightCRRA](http://econ.jhu.edu/people/ccarroll/public/lecturenotes/consumption/PerfForesightCRRA/)) that the perfect foresight 'Growth Impatience Factor' is 
+# \begin{align}\label{eq:PFGIC}
+# \left(\frac{(R \beta)^{1/\rho}}{G}\right)  &<  1
+# \end{align}
+# so since $\mho > 0$, uncertainty makes it harder to be 'impatient.'
 #
+# Think of someone who, in the perfect foresight model, was 'poised': Exactly on the knife edge between patience and impatience.  Now add a precautionary saving motive; that person will now (to some degree) be pushed off the knife edge in the direction of 'patience.'  So, in the presence of uncertainty, the conditions on parameters other than $\mho$ must be stronger in order to guarantee 'impatience' in the sense of wanting to spend enough for your wealth to decline _despite_ the extra precautionary motive.
 
-# %% {"code_folding": [0]}
+# %% {"code_folding": [0, 3]}
 # Make an interactive plot of the tractable buffer stock solution
 
 # To make some of the widgets not appear, replace X_widget with fixed(desired_fixed_value) in the arguments below.
@@ -244,4 +255,5 @@ interact(makeTBSplot,
          plot_ret = plot_ret_widget,
          plot_mSS = plot_mSS_widget,
         );
+
 
