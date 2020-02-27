9780ef2e396a6899d113b721410b5b967bbdd180
llorracc <@users.noreply.github.com>
2020-02-27 22:36:48 +0100

    Fix aNrm vs aLev error

--- notebooks/LifecycleModelExample.py
+++ notebooks/LifecycleModelExample.py
@@ -16,26 +16,15 @@
 
 # %% [markdown]
 #
-# # The Distribution of Assets By Age
+# # A Life Cycle Model: Data and Theory
 #
-# National registry data on income and wealth from Scandinavian countries has recently become available (with a lot of security) to some (lucky!) researchers.   These data offer a uniquely powerful tool for testing (and improving) our models of consumption and saving behavior over the life cycle.
+# National registry data on income and wealth from Scandinavian countries (esp. Norway) have recently become available (with a lot of security) to some (lucky!) researchers.   These data offer a uniquely powerful tool for testing (and improving) our models of consumption and saving behavior over the life cycle.
 #
+# This notebook is an example of how to construct a life cycle model with the HARK toolkit that makes predictions that can be compared to the raw data statistics that now are becoming available.
 #
-# But as of this writing (in March of 2019), the data are so new that there do not seem to be any published attempts to compare the data to the implications a standard life cycle model with income uncertainty, constraints, and other modern features.
+# For example, existing papers have tabulated information about the **growth rate** of assets at different ages over the life cycle. 
 #
-# This notebook is an example of how one could counstruct a life cycle model with the HARK toolkit that would make predictions about the model analogues of the raw data statistics that are available.  
-#
-# For example, the existing papers have tabulated information about the **growth rate** of assets at different ages over the life cycle.  Here, we show how (under a given parameterization) we could produce the life cycle model's prediction about the **distribution** of assets at age 65 and age 66, and the growth rate between 65 and 66. 
-#
-# The parameters of the model have not been optmized to match features of the Norwegian data; a first step in "structural" estimation would be to calibrate the inputs to the model (like the profile of income over the life cycle, and the magnitude of income shocks), and then to find the values of parameters like the time preference rate that allow the model to fit the data best.
-#
-# An interesting question is whether this exercise will suggest that it is necessary to allow for _ex ante_ heterogeneity in such preference parameters.
-#
-# This seems likely; a paper by [<cite data-cite="6202365/7MR8GUVS"></cite>](http://econ.jhu.edu/people/ccarroll/papers/cstwMPC) (all of whose results were constructed using the HARK toolkit) finds that, if all other parameters (e.g., rates of return on savings) are the same, models of this kind require substantial heterogeneity in preferences to generate the degree of inequality in U.S. data.
-#
-# But in one of the many new and interesting findings from the Norwegian data, <cite data-cite="6202365/B9BGV9W3"></cite> have shown that there is substantial heterogeneity in rates of return, even on wealth held in public markets.  
-#
-# [Derin Aksit](https://github.com/econ-ark/REMARK) has shown that the degree of time preference heterogeneity needed to match observed inequality is considerably less when rate-of-return heterogeneity is calibrated to match these data.
+# The default parameters of the HARK life cycle model have not been optmized to match features of the Norwegian data; a first step in a real "structural" estimation would be to use Norwegian calibrate the inputs to the model (like the profile of income, and the magnitude of income shocks, over the life cycle), and then to find the values of parameters like the time preference rate that allow the model to fit the data best.  (See [SolvingMicroDSOPs](https://econ.jhu.edu/people/ccarroll/SolvingMicroDSOPs) for how this can be done, and search for the corresponding HARK content using [our documentation](https://hark.readthedocs.io)).
 
 # %% {"code_folding": [0]}
 # Initial imports and notebook setup, click arrow to show
@@ -48,18 +37,16 @@ import numpy as np
 
 
 # %% {"code_folding": [0]}
-# Set up default values for CRRA, DiscFac, and simulation variables in the dictionary 
-Params.init_consumer_objects["CRRA"]= 2.00            # Default coefficient of relative risk aversion (rho)
-Params.init_consumer_objects["DiscFac"]= 0.97         # Default intertemporal discount factor (beta)
+# Set up values for simulation parameters in the dictionary dictionary 
 Params.init_consumer_objects["PermGroFacAgg"]= 1.0    # Aggregate permanent income growth factor 
-Params.init_consumer_objects["aNrmInitMean"]= -10.0   # Mean of log initial assets 
+Params.init_consumer_objects["aNrmInitMean"]= -10.0   # Mean of log initial assets (\approx = 0)
 Params.init_consumer_objects["aNrmInitStd"]= 1.0      # Standard deviation of log initial assets
-Params.init_consumer_objects["pLvlInitMean"]= 0.0     # Mean of log initial permanent income 
+Params.init_consumer_objects["pLvlInitMean"]= 0.0     # Mean log initial permanent income (everyone starts with log(pLvl)=0)
 Params.init_consumer_objects["pLvlInitStd"]= 0.0      # Standard deviation of log initial permanent income
 
 
 # %%
-# Make a lifecycle consumer to be used for estimation
+# Make an instance of a lifecycle consumer to be used for estimation
 LifeCyclePop = cShksModl.IndShockConsumerType(**Params.init_consumer_objects)
 
 
@@ -69,7 +56,7 @@ LifeCyclePop.solve()                            # Obtain consumption rules by ag
 LifeCyclePop.unpackcFunc()                      # Expose the consumption rules
 
 # Which variables do we want to track
-LifeCyclePop.track_vars = ['aNrmNow','pLvlNow','mNrmNow','cNrmNow','TranShkNow','pLvlNow','TranShkNow']
+LifeCyclePop.track_vars = ['aNrmNow','pLvlNow','mNrmNow','cNrmNow','TranShkNow','pLvlNow']
 
 LifeCyclePop.T_sim = 120                        # Nobody lives to be older than 145 years (=25+120)
 LifeCyclePop.initializeSim()                    # Construct the age-25 distribution of income and assets
@@ -106,8 +93,8 @@ def savRteFunc(SomeType, m):
     return savRte  
 
 
-# %% {"code_folding": [0]}
-# Create a Giant matrix gathering useful data:
+# %% {"code_folding": []}
+# Create a giant matrix gathering useful data:
 # 't_now', 'aNrmNow_hist', 'cNrmNow_hist', employment-status in date t and date t-1,
 # aLvlGro_hist, Saving rate
 
@@ -120,12 +107,13 @@ warnings.filterwarnings("ignore") # Suppress some disturbing but harmless warnin
 
 for t in range(1,LifeCyclePop.T_cycle+1):
     #aLvlGro_hist[0] = 0 # set the first growth rate to 0, since there is no data for period 0
-    aLvlGroNow = np.log(LifeCyclePop.aNrmNow_hist[t]/LifeCyclePop.aNrmNow_hist[t-1]) # (10000,)
+    aLvlGroNow = np.log((LifeCyclePop.aNrmNow_hist[t]   *LifeCyclePop.pLvlNow_hist[t])/ \
+                         LifeCyclePop.aNrmNow_hist[t-1])* # (10000,)
 
-    # Call the saving rate function with test value for 
+    # Call the saving rate function defined above 
     savRte = savRteFunc(LifeCyclePop, LifeCyclePop.mNrmNow_hist[t] )
       
-    savRte_list.append(savRte)
+    savRte_list.append(savRte) # Add this period's saving rate to the list 
 
     # Create elements of matrix list
     matrix_list = [0 for number in range(7)]
@@ -139,17 +127,28 @@ for t in range(1,LifeCyclePop.T_cycle+1):
     
     giant_list[t-1] = matrix_list
 
-
-
 # %% {"code_folding": [0]}
 # Construct the level of assets A from a*p where a is the ratio to permanent income p
+# Remember 41 is "years after entering workforce" (=age 25); 66 is the year right after retirement
 LifeCyclePop.aLvlNow_hist = LifeCyclePop.aNrmNow_hist*LifeCyclePop.pLvlNow_hist
 aGro41=LifeCyclePop.aLvlNow_hist[41]/LifeCyclePop.aLvlNow_hist[40]
-aGro41NoU=aGro41[aGro41[:]>0.2] # Throw out extreme outliers
+aGro41NoU=aGro41[aGro41[:]>0.2] # Throw out extreme outliers; don't want growth rates relative to 0 income!
 
 
 # %% {"code_folding": [0]}
-# Plot the distribution of growth rates of wealth between age 65 and 66 (=25 + 41)
+# Plot the (truncated) distribution of growth rates of wealth between age 65 and 66 (=25 + 41)
 from matplotlib import pyplot as plt
 n, bins, patches = plt.hist(aGro41NoU,50,density=True)
 
+
+# %%
+# put your solution here
+
+# %%
+# put your answer here
+
+# %%
+# put your answer here
+
+# %%
+# put your solution here
