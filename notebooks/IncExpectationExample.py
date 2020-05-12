547ab984d9168cb148c5b688ac2fee52e03dda29
sb <sbenthall@gmail.com>
2020-05-12 14:57:14 -0400

    fixing DemARKs that used approxUniform

--- notebooks/IncExpectationExample.py
+++ notebooks/IncExpectationExample.py
@@ -54,7 +54,7 @@ from copy import copy
 import numpy as np
 
 from HARK.ConsumptionSaving.ConsGenIncProcessModel import PersistentShockConsumerType
-from HARK.distribution import approxUniform
+from HARK.distribution import Uniform
 from HARK.utilities import getLorenzShares, calcSubpopAvg
 mystr = lambda number : "{:.4f}".format(number)
 
@@ -149,7 +149,7 @@ def runRoszypalSchlaffmanExperiment(CorrAct, CorrPcvd, DiscFac_center, DiscFac_s
     ThisDict['PrstIncCorr'] = CorrAct
     
     # Make a 7 point approximation to a uniform distribution of DiscFac
-    DiscFac_list = approxUniform(N=7,bot=DiscFac_center-DiscFac_spread,top=DiscFac_center+DiscFac_spread).X
+    DiscFac_list = Uniform(bot=DiscFac_center-DiscFac_spread,top=DiscFac_center+DiscFac_spread).approx(N=7).X
     
     type_list = []
     # Make a PersistentShockConsumerTypeX for each value of beta saved in DiscFac_list
--- notebooks/Nondurables-During-Great-Recession.py
+++ notebooks/Nondurables-During-Great-Recession.py
@@ -126,12 +126,12 @@ for nn in range(num_consumer_types):
 
 # %% {"code_folding": [0]}
 # Seven types is enough to approximate the uniform distribution (5 is not quite enough)
-from HARK.distribution import approxUniform
+from HARK.distribution import Uniform
 
 # Calibrations from cstwMPC
 bottomDiscFac  = 0.9800
 topDiscFac     = 0.9934
-DiscFac_list   = approxUniform(N=num_consumer_types,bot=bottomDiscFac,top=topDiscFac).X
+DiscFac_list   = Uniform(bot=bottomDiscFac,top=topDiscFac).approx(N=num_consumer_types).X
 
 # Now, assign the discount factors
 for j in range(num_consumer_types):
