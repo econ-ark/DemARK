81001df19fffee3e4870337a660179dea2f42a92
MateoVG <mateovegi@gmail.com>
2020-11-03 16:24:18 -0500

    Revise and add tests of analytical solutions

--- notebooks/LucasAssetPrice-With-AR1-Dividends.py
+++ notebooks/LucasAssetPrice-With-AR1-Dividends.py
@@ -7,7 +7,7 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.2'
-#       jupytext_version: 1.2.1
+#       jupytext_version: 1.2.4
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
@@ -17,13 +17,13 @@
 # %% [markdown]
 # # Lucas Pricing of AR1 trees
 #
-# ## A notebook by [Mateo Velásquez-Giraldo](https://mv77.github.io/)
+# ## A notebook by [Christopher D. Carroll](http://www.econ2.jhu.edu/people/ccarroll/) and [Mateo Velásquez-Giraldo](https://mv77.github.io/)
 # ### Inspired by its [Quantecon counterpart](https://julia.quantecon.org/multi_agent_models/lucas_model.html)
 #
 # This notebook presents simple computational tools to solve Lucas' asset-pricing model when the logarithm of the asset's dividend follows an autoregressive process of order 1,
 #
 # \begin{equation*}
-# \ln d_{t+1} = \alpha \ln d_t + \varepsilon_{t+1}
+# \ln d_{t+1} = \alpha \ln d_t + \varepsilon_{t+1}.
 # \end{equation*}
 #
 # A presentation of this model can be found in [Christopher D. Carroll's lecture notes](http://www.econ2.jhu.edu/people/ccarroll/public/lecturenotes/AssetPricing/LucasAssetPrice/). 
@@ -46,10 +46,13 @@
 #
 # | Process | CRRA | Solution for Pricing Kernel | 
 # | --- | --- | --- |
-# | IID | 1 (log) | $P(d) = \frac{d}{\theta}$ |
-# | IID | $\rho$ | $P(d) = \frac{d}{\theta-\rho(\rho-1)\sigma^{2}/2}$ |
+# | IID | 1 (log) | $P^*(d) = \frac{d}{\theta}$ |
+# | IID | $\rho$ | $P^*(d) \approx \frac{d^\rho}{\theta-\rho(\rho-1)\sigma^{2}/2}$ |
 #
 # However, under less special circumstances, the only way to obtain the pricing function $P^{*}$ is by solving for it numerically, as below.
+
+# %% [markdown]
+# # A computational representation of the problem and its solution.
 
 # %% Preamble {"code_folding": [0]}
 # Setup
@@ -61,19 +64,30 @@ from HARK.utilities import CRRAutilityP
 from HARK.distribution import Normal
 from HARK.interpolation import LinearInterp, ConstantFunction
 
-# %% Definitions {"code_folding": []}
-# Define the model
+# %% Definitions {"code_folding": [0]}
+# A class representing log-AR1 dividend processes.
 class DivProcess:
     
-    def __init__(self, alpha, shock_sd, nApprox = 7):
+    def __init__(self, alpha, shock_sd, shock_mean = 0.0, nApprox = 7):
         
         self.alpha = alpha
         self.shock_sd = shock_sd
+        self.shock_mean = shock_mean
         self.nApprox = nApprox
         
         # Create a discrete approximation to the random shock
-        self.ShkAppDstn = Normal(sigma = shock_sd).approx(N = nApprox)
+        self.ShkAppDstn = Normal(mu = shock_mean, sigma = shock_sd).approx(N = nApprox)
         
+    def getLogdGrid(self):
+        '''
+        A method for creating a reasonable grid for log-dividends.
+        '''
+        uncond_sd = self.shock_sd / np.sqrt(1 - self.alpha**2)
+        uncond_mean = self.shock_mean/(1-self.alpha)
+        logDGrid = np.linspace(-5*uncond_sd, 5*uncond_sd, 100) + uncond_mean
+        return(logDGrid)
+        
+# A class representing economies with Lucas' trees.
 class LucasEconomy:
     
     def __init__(self, CRRA, DiscFac, DivProcess):
@@ -83,17 +97,6 @@ class LucasEconomy:
         self.DivProcess = DivProcess
         self.uP = lambda c: CRRAutilityP(c, self.CRRA)
         
-
-
-# %% [markdown]
-# Suppose we have a hypothesized pricing kernel for period $t+1$.
-#
-# \begin{equation*}
-# P_{t}(d_t) = \left(\frac{1}{1+\theta}\right)\mathbb{E}_{t}\left[ \frac{u^{\prime}(d_{t+1})}{u^{\prime}(d_t)} (P_{t+1}(d_{t+1}) + d_{t+1}) \right]
-# \end{equation*}
-#
-
-# %% Definitions {"code_folding": []}
     def priceOnePeriod(self, Pfunc_next, logDGrid):
         
         # Create 'tiled arrays' rows are state today, columns are value of
@@ -141,8 +144,7 @@ class LucasEconomy:
         
         # Create a grid for log-dividends if one is not provided
         if logDGrid is None:
-            uncond_sd = self.DivProcess.shock_sd / np.sqrt(1 - self.DivProcess.alpha**2)
-            logDGrid = np.linspace(-5*uncond_sd, 5*uncond_sd, 100)
+            logDGrid = self.DivProcess.getLogdGrid()
         
         # Initialize function and compute prices on the grid
         Pf_0 = copy(Pfunc_0)
@@ -174,8 +176,9 @@ class LucasEconomy:
         self.EqlogPfun = Pf_0
         self.EqPfun = lambda d: self.EqlogPfun(np.log(d))
 
+
 # %% [markdown]
-# # An example economy
+# # Creating and solving an example economy
 #
 # An economy is fully specified by:
 # - **The dividend process for the assets (trees)**: we assume that $\ln d_{t+1} = \alpha \ln d_t + \varepsilon_{t+1}$. We must create a dividend process specifying $\alpha$ and $\sigma_{\varepsilon}$.
@@ -191,7 +194,7 @@ economy = LucasEconomy(CRRA = 2, DiscFac = 0.95, DivProcess = DivProc)
 
 
 # %% [markdown]
-# Once created, the economy can be 'solved', which means finding the equilibrium price function. The distribution of dividends at period $t+1$ depends on the value of dividends at $t$. Thus, $d_t$ is a state variable for the economy. The pricing function gives the price of trees that equates their demand and supply at every level of current dividends $d_t$.
+# Once created, the economy can be 'solved', which means finding the equilibrium price kernel. The distribution of dividends at period $t+1$ depends on the value of dividends at $t$. Thus, $d_t$ is a state variable for the economy. The pricing function gives the price of trees that equates their demand and supply at every level of current dividends $d_t$.
 
 # %% Solution
 # Solve the economy
@@ -203,7 +206,9 @@ print('P({}) = {}'.format(d, economy.EqPfun(d)))
 
 
 # %% [markdown]
-# ## The effect of risk aversion
+# ## The effect of risk aversion.
+#
+# [The notes](http://www.econ2.jhu.edu/people/ccarroll/public/lecturenotes/AssetPricing/LucasAssetPrice/) discuss the surprising implication that an increase in the coefficient of relative risk aversion $\rho$ leads to higher prices for the risky trees! This is demonstrated below.
 
 # %%
 # Create two economies with different risk aversion
@@ -223,8 +228,67 @@ plt.legend()
 plt.xlabel('$d_t$')
 plt.ylabel('$P_t$')
 
-# %%
-import HARK
-HARK.__file__
+# %% [markdown]
+# # Testing our analytical solutions
+
+# %% [markdown]
+# ## 1. Log-utility
+#
+# The lecture notes show that with log-utility (a CRRA of $1$), the pricing kernel has a closed form expression: $$P^*(d_t) = \frac{d_t}{\vartheta}$$.
+#
+# We now compare our numerical solution with this analytical expression.
 
 # %%
+# Create an economy with log utility and the same dividend process from before
+logUtilEcon = LucasEconomy(CRRA = 1, DiscFac = Disc, DivProcess = DivProc)
+# Solve it
+logUtilEcon.solve()
+
+# Generate a function with our analytical solution
+theta = 1/Disc - 1
+aSol = lambda d: d/theta
+
+# Get a grid for d over which to compare them
+dGrid = np.exp(DivProc.getLogdGrid())
+
+# Plot both
+plt.plot(dGrid, aSol(dGrid), '*',label = 'Analytical solution')
+plt.plot(dGrid, logUtilEcon.EqPfun(dGrid), label = 'Numerical solution')
+plt.legend()
+plt.xlabel('$d_t$')
+plt.ylabel('$P^*(d_t)$')
+
+# %% [markdown]
+#  ## 2. I.I.D dividends
+#  
+#  We also found that, if $\ln d_{t+n}\sim \mathcal{N}(-\sigma^2/2, \sigma^2)$ for all $n$, the pricing kernel can me approximated by
+#  \begin{equation*}
+#  P^*(d_t)\approx \frac{d_t^\rho}{\vartheta - \rho(\rho-1)\sigma^2/2}.
+#  \end{equation*}
+#  We now test this approximation.
+
+# %%
+# Create an i.i.d dividend process
+shock_sd = 0.1
+iidDivs = DivProcess(alpha = 0.0, shock_mean = -shock_sd**2/2, shock_sd = shock_sd)
+
+# And an economy that embeds it
+CRRA = 2
+Disc = 0.9
+
+iidEcon = LucasEconomy(CRRA = CRRA, DiscFac = Disc, DivProcess = iidDivs)
+iidEcon.solve()
+
+# Generate a function with our analytical solution
+theta = 1/Disc - 1
+aSolIID = lambda d: d**CRRA/(theta - CRRA*(CRRA-1)*shock_sd**2/2)
+
+# Get a grid for d over which to compare them
+dGrid = np.exp(iidDivs.getLogdGrid())
+
+# Plot both
+plt.plot(dGrid, aSolIID(dGrid), '*',label = 'Analytical solution')
+plt.plot(dGrid, iidEcon.EqPfun(dGrid), label = 'Numerical solution')
+plt.legend()
+plt.xlabel('$d_t$')
+plt.ylabel('$P^*(d_t)$')
