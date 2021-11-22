e6817575285d52e993d3e2e79bb956ce85c63fe9
Mateo <mateovegi@gmail.com>
2021-11-22 10:58:02 -0500

    Outline

---
+++ notebooks/Harmenberg-Aggregation.py
@@ -0,0 +1,26 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.13.0
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # Description of the problem
+
+# %% [markdown]
+# # Description of the method
+
+# %% [markdown]
+# # Demonstration of HARK's implementation and the gain in efficiency
+
+# %% [markdown]
+# # Comparison of the PIN-measure and the base measure
