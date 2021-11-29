d93762e3553d74661c848654c975b2bf40782d97
MateoVG <mateovegi@gmail.com>
2021-11-29 14:31:22 -0500

    Use command definitions

--- notebooks/Harmenberg-Aggregation.py
+++ notebooks/Harmenberg-Aggregation.py
@@ -7,9 +7,9 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.13.0
+#       jupytext_version: 1.13.1
 #   kernelspec:
-#     display_name: Python 3 (ipykernel)
+#     display_name: Python 3
 #     language: python
 #     name: python3
 # ---
@@ -19,7 +19,16 @@
 #
 # ## Author: [Mateo Velásquez-Giraldo](https://mv77.github.io/)
 
-# %%
+# %% [markdown]
+# Symbol definitions
+# $\newcommand{\PInc}{P}$
+# $\newcommand{\aggC}{\bar{\mathbf{C}}}$
+# $\newcommand{\aggM}{\bar{\mathbf{M}}}$
+# $\newcommand{\aggCest}{\widehat{\aggC}}$
+# $\newcommand{\aggMest}{\widehat{\aggM}}$
+# $\newcommand{\mPdist}{\psi}$
+
+# %% code_folding=[]
 # Preliminaries
 from HARK.ConsumptionSaving.ConsIndShockModel import (
     IndShockConsumerType
@@ -36,10 +45,10 @@ from matplotlib import pyplot as plt
 # %% [markdown]
 # # Description of the problem
 #
-# In macroeconomic models with heterogeneous agents, the permanent component of agents' income ($\textbf{P}_t$) often follows a geometric random walk. At any time, these economies include agents with different levels of permanent income. To find an aggregate characteristic of these economies such as aggregate consumption $\bar{\textbf{C}}_t$, one must integrate over permanent income and all the other relevant state variables
+# In macroeconomic models with heterogeneous agents, the permanent component of agents' income ($\PInc_t$) often follows a geometric random walk. At any time, these economies include agents with different levels of permanent income. To find an aggregate characteristic of these economies such as aggregate consumption $\aggC_t$, one must integrate over permanent income and all the other relevant state variables
 #
 # \begin{equation*}
-# \bar{\textbf{C}}_t = \int \int C(\text{State},\textbf{P}) \times f_t(\text{State},\textbf{P}) \, d\text{State}\, d\textbf{P}, 
+# \aggC_t = \int \int C(\text{State},\PInc) \times f_t(\text{State},\PInc) \, d\text{State}\, d\PInc, 
 # \end{equation*}
 #
 # where $\text{State}$ denotes any other state variables that consumption might depend on, $C(\cdot,\cdot)$ is the individual consumption function, and $f_t(\cdot,\cdot)$ is the joint density function of permanent income and the other state variables at time $t$.
@@ -47,38 +56,38 @@ from matplotlib import pyplot as plt
 # Models like the traditional Buffer-Stock-Saving agent with CRRA utility [CITE CARROLL 2021] are homothetic in permanent income. This means that one can solve for a normalized policy function $c(\cdot)$ such that
 #
 # \begin{equation*}
-#     C(\text{State},\textbf{P}) = c\left(\frac{1}{\textbf{P}}\times \text{State}\right)\times \textbf{P} \qquad \forall \, (\text{State},\textbf{P}).
+#     C(\text{State},\PInc) = c\left(\frac{1}{\PInc}\times \text{State}\right)\times \PInc \qquad \forall \, (\text{State},\PInc).
 # \end{equation*}
 #
-# In practice, this implies that one can defined a normalized state vector $\widetilde{\text{State}} = \text{State}/\textbf{P}$ and solve for the normalized policy function. This eliminates one dimension of the optimization problem problem, $\textbf{P}$.
+# In practice, this implies that one can defined a normalized state vector $\widetilde{\text{State}} = \text{State}/\PInc$ and solve for the normalized policy function. This eliminates one dimension of the optimization problem problem, $\PInc$.
 #
 # While convenient for the solution of the agents' optimization problem, homotheticity has not simplified our aggregation calculations as we still have
 #
 # \begin{equation*}
 # \begin{split}
-# \bar{\textbf{C}}_t =& \int \int C(\text{State},\textbf{P}) \times f_t(\text{State},\textbf{P}) \, d\text{State}\, d\textbf{P}\\
-# =& \int \int c\left(\frac{1}{\textbf{P}}\times \text{State}\right)\times \textbf{P} \times f_t(\text{State},\textbf{P}) \, d\text{State}\, d\textbf{P},
+# \aggC_t =& \int \int C(\text{State},\PInc) \times f_t(\text{State},\PInc) \, d\text{State}\, d\PInc\\
+# =& \int \int c\left(\frac{1}{\PInc}\times \text{State}\right)\times \PInc \times f_t(\text{State},\PInc) \, d\text{State}\, d\PInc,
 # \end{split}
 # \end{equation*}
 #
 # which depends on $P$.
 #
-# To further complicate matters, we usually do not have analytical expressions for $c(\cdot)$ or $f_t(\text{State},\textbf{P})$. What we do in practice is to simulate a population $I$ of agents for a large number of periods $T$ using the model's policy functions and transition equations. The result is a set of observations $\{\text{State}_{i,t},\textbf{P}_{i,t}\}_{i\in I, 0\leq t\leq T}$ which we then use to approximate
+# To further complicate matters, we usually do not have analytical expressions for $c(\cdot)$ or $f_t(\text{State},\PInc)$. What we do in practice is to simulate a population $I$ of agents for a large number of periods $T$ using the model's policy functions and transition equations. The result is a set of observations $\{\text{State}_{i,t},\PInc_{i,t}\}_{i\in I, 0\leq t\leq T}$ which we then use to approximate
 #
 # \begin{equation*}
-# \bar{\textbf{C}}_t \approx \frac{1}{|I|}\sum_{i \in I} c\left(\frac{1}{\textbf{P}_{i,t}}\times \text{State}_{i,t}\right)\times \textbf{P}_{i,t}. 
+# \aggC_t \approx \frac{1}{|I|}\sum_{i \in I} c\left(\frac{1}{\PInc_{i,t}}\times \text{State}_{i,t}\right)\times \PInc_{i,t}. 
 # \end{equation*}
 #
 # At least two features of the previous strategy are unpleasant:
 # - We have to simulate the distribution of permanent income, even though the model's solution does not depend on it.
-# - As a geometric random walk, permanent income might have an unbounded distribution with a thick right tail. Since $\textbf{P}_{i,t}$ appears multiplicatively in our approximation, agents with high permanent incomes will be the most important to our results. Therefore, it is important for our simulated population to achieve a good approximation of the distribution of permanent income in its thick right tail, which will require us to use many agents.
+# - As a geometric random walk, permanent income might have an unbounded distribution with a thick right tail. Since $\PInc_{i,t}$ appears multiplicatively in our approximation, agents with high permanent incomes will be the most important to our results. Therefore, it is important for our simulated population to achieve a good approximation of the distribution of permanent income in its thick right tail, which will require us to use many agents.
 #
-# [CITE HARMENBERG 2021] presents a way to resolve the previous two points. His solution constructs a distribution $\widetilde{\psi}_t(\cdot)$ of the normalized state vector that he calls **the permanent-income neutral measure** and which has the convenient property that
+# [CITE HARMENBERG 2021] presents a way to resolve the previous two points. His solution constructs a distribution $\widetilde{f}_t(\cdot)$ of the normalized state vector that he calls **the permanent-income neutral measure** and which has the convenient property that
 #
 # \begin{equation*}
 # \begin{split}
-# \bar{\textbf{C}}_t =& \int \int c\left(\frac{1}{\textbf{P}}\times \text{State}\right)\times \textbf{P} \times f_t(\text{State},\textbf{P}) \, d\text{State}\, d\textbf{P}\\
-# =& \int c\left(\widetilde{\text{State}}\right) \times \widetilde{\psi}(\widetilde{\text{State}}) \, d\widetilde{\text{State}}
+# \aggC_t =& \int \int c\left(\frac{1}{\PInc}\times \text{State}\right)\times \PInc \times f_t(\text{State},\PInc) \, d\text{State}\, d\PInc\\
+# =& \int c\left(\widetilde{\text{State}}\right) \times \widetilde{f}(\widetilde{\text{State}}) \, d\widetilde{\text{State}}
 # \end{split}
 # \end{equation*}
 #
@@ -91,24 +100,24 @@ from matplotlib import pyplot as plt
 #
 # To illustrate Harmenberg's idea, consider a model in which:
 # - The indivudual agent's problem has two state variables:
-#     - His market resources $\textbf{m}_{i,t}$.
+#     - His market resources $\mathbf{m}_{i,t}$.
 #     - His permanent income $P_{i,t}$.
 #     
-# - The agent's problem is homothetic in his permanent income, so that we can define $m_t = \textbf{m}_t/P_t$ and find a normalized policy function $c(\cdot)$ such that $$c(\frac{\textbf{m}_t}{P_t})*P_t = \mathbf{c}(\textbf{m}_t, P_t)\quad \forall(\textbf{m}_t, P_t)$$ where $\mathbf{c}(\cdot,\cdot)$ is the optimal consumption function.
+# - The agent's problem is homothetic in his permanent income, so that we can define $m_t = \mathbf{m}_t/P_t$ and find a normalized policy function $c(\cdot)$ such that $$c(\frac{\mathbf{m}_t}{P_t})*P_t = \mathbf{c}(\mathbf{m}_t, P_t)\quad \forall(\mathbf{m}_t, P_t)$$ where $\mathbf{c}(\cdot,\cdot)$ is the optimal consumption function.
 #
 # - $P_t$ evolves according to $$P_{t+1} = \Gamma \eta_{t+1} P_t,$$ where $\eta_{t+1}$ is a shock with density function $f_\eta(\cdot)$ satisfying $E_t[\eta_{t+1}] = 1$.
 #
 # To compute aggregate consumption $\bar{C}_t$ in this model, we would follow the approach from above
 # \begin{equation*}
-# \bar{C}_t = \int \int c(m)\times\textbf{P} \times \psi_t(m,\textbf{P}) \, d\text{m}\, d\textbf{P},
+# \bar{C}_t = \int \int c(m)\times\PInc \times \psi_t(m,\PInc) \, d\text{m}\, d\PInc,
 # \end{equation*}
-# where $\psi_t(m,\textbf{P})$ is the measure of agents with normalized resources $m$ and permanent income $P$.
+# where $\mPdist_t(m,\PInc)$ is the measure of agents with normalized resources $m$ and permanent income $P$.
 #
 # The first of Harmenberg's insights is that the previous integral can be rearranged as
 # \begin{equation*}
-# \bar{C}_t = \int c(m)\left(\int \textbf{P} \times \psi_t(m,\textbf{P}) d\textbf{P}\right) \, d\text{m}.
+# \bar{C}_t = \int c(m)\left(\int \PInc \times \mPdist_t(m,\PInc) d\PInc\right) \, d\text{m}.
 # \end{equation*}
-# The inner integral, $\int \textbf{P} \times \psi_t(m,\textbf{P}) d\textbf{P}$, is a function of $m$ and it measures *the total amount of permanent income accruing to agents with normalizer market resources of* $m$.
+# The inner integral, $\int \PInc \times \mPdist_t(m,\PInc) d\PInc$, is a function of $m$ and it measures *the total amount of permanent income accruing to agents with normalizer market resources of* $m$.
 
 # %% [markdown]
 # # Harmenberg's method in HARK
@@ -117,7 +126,7 @@ from matplotlib import pyplot as plt
 #
 # As the cell below illustrates, using Harmenberg's method in HARK simply requires to set an agent's property `agent.neutral_measure = True` and then update his income process. After these steps, `agent.simulate` will simulate the model using Harmenberg's permanent-income-neutral measure.
 
-# %%
+# %% code_folding=[0]
 # Create an infinite horizon agent with the default parametrization
 example = IndShockConsumerType(**dict_harmenberg, verbose = 0)
 example.cycles = 0
@@ -143,42 +152,42 @@ example.simulate()
 #
 # To demonstrate the gain in efficiency from using Harmenberg's method, we will set up the following experiment.
 #
-# Consider an economy populated by [Buffer-Stock agents](https://llorracc.github.io/BufferStockTheory/), whose individual-level state variables are market resources $\textbf{m}_t$ and permanent income $\textbf{P}_t$. [CITE BUFFER STOCK THEORY] shows that this type of agents has an homothetic consumption function, so that we can define normalized market resources $m_t \equiv \textbf{m}_t / \textbf{P}_t$, solve for a normalized consumption function $c(\cdot)$, and express the consumption function as $\textbf{c}(\textbf{m},\textbf{p}) = c(m)\times\textbf{P}$.
+# Consider an economy populated by [Buffer-Stock agents](https://llorracc.github.io/BufferStockTheory/), whose individual-level state variables are market resources $\mathbf{m}_t$ and permanent income $\PInc_t$. [CITE BUFFER STOCK THEORY] shows that this type of agents has an homothetic consumption function, so that we can define normalized market resources $m_t \equiv \mathbf{m}_t / \PInc_t$, solve for a normalized consumption function $c(\cdot)$, and express the consumption function as $\mathbf{c}(\mathbf{m},\PInc) = c(m)\times\PInc$.
 #
-# Assume further that mortality, impatience, and permanent income growth are such that the economy converges to stable joint distribution of $m$ and $\textbf{P}$ characterized by the density function $f(\cdot,\cdot)$. Under these conditions, define the stable level of aggregate market resources and consumption as
+# Assume further that mortality, impatience, and permanent income growth are such that the economy converges to stable joint distribution of $m$ and $\PInc$ characterized by the density function $\mPdist(\cdot,\cdot)$. Under these conditions, define the stable level of aggregate market resources and consumption as
 #
 # \begin{equation}
-#     \bar{\textbf{M}} \equiv \int \int m \times \textbf{P} \times f(m, \textbf{P})\,dm\,d\textbf{P}, \qquad
-#     \bar{\textbf{C}} \equiv \int \int c(m) \times \textbf{P} \times f(m, \textbf{P})\,dm\,d\textbf{P}.
+#     \aggM \equiv \int \int m \times \PInc \times \mPdist(m, \PInc)\,dm\,d\PInc, \qquad
+#     \aggC \equiv \int \int c(m) \times \PInc \times \mPdist(m, \PInc)\,dm\,d\PInc.
 # \end{equation}
 #
-# If we could simulate the economy with a continuum of agents we would find that, over time, our estimate of aggregate market resources $\hat{\bar{\textbf{M}}}_t$ would converge to $\bar{\textbf{M}}$ and our estimate of aggregate consumption $\hat{\bar{\textbf{C}}}_t$ would converge to $\bar{\textbf{C}}$. Therefore, if we computed our aggregate estimates at different periods in time we would find them to be close:
+# If we could simulate the economy with a continuum of agents we would find that, over time, our estimate of aggregate market resources $\aggMest_t$ would converge to $\aggM$ and our estimate of aggregate consumption $\aggCest_t$ would converge to $\aggC$. Therefore, if we computed our aggregate estimates at different periods in time we would find them to be close:
 # \begin{equation}
-#     \hat{\bar{\textbf{M}}}_t \approx \hat{\bar{\textbf{M}}}_{t+n} \approx \bar{\textbf{M}}\quad
-#     \text{and} \quad
-#     \hat{\bar{\textbf{C}}}_t \approx \hat{\bar{\textbf{C}}}_{t+n} \approx \bar{\textbf{C}},\quad
+#     \aggMest_t \approx \aggMest_{t+n} \approx \aggM\qquad
+#     \text{ and } \qquad
+#     \aggCest_t \approx \aggCest_{t+n} \approx \aggC,\qquad
 #     \text{for } n>0 \text{ and } t \text{ large enough}.
 # \end{equation}
 #
 # In practice, however, we rely on approximations using a finite number of agents $I$. Our estimates of aggregate market resources and consumption at time $t$ are
 #
 # \begin{equation}
-# \hat{\bar{\textbf{M}}}_t \equiv \frac{1}{I} \sum_{i=1}^{I} m_{i,t}\times\textbf{P}_{i,t}, \quad \hat{\bar{\textbf{C}}}_t \equiv \frac{1}{I} \sum_{i=1}^{I} c(m_{i,t})\times\textbf{P}_{i,t},
+# \aggMest_t \equiv \frac{1}{I} \sum_{i=1}^{I} m_{i,t}\times\PInc_{i,t}, \quad \aggCest_t \equiv \frac{1}{I} \sum_{i=1}^{I} c(m_{i,t})\times\PInc_{i,t},
 # \end{equation}
 #
 # under the basic simulation strategy or
 #
 # \begin{equation}
-# \hat{\bar{\textbf{M}}}_t \equiv \frac{1}{I} \sum_{i=1}^{I} \tilde{m}_{i,t}, \quad \hat{\bar{\textbf{C}}}_t \equiv \frac{1}{I} \sum_{i=1}^{I} c(\tilde{m}_{i,t}),
+# \aggMest_t \equiv \frac{1}{I} \sum_{i=1}^{I} \tilde{m}_{i,t}, \quad \aggCest_t \equiv \frac{1}{I} \sum_{i=1}^{I} c(\tilde{m}_{i,t}),
 # \end{equation}
 #
 # if we use Harmenberg's method to simulate the distribution of normalized market resources under the permanent-income neutral measure.
 #
-# If we do not use enough agents, our distributions of agents over state variables will be inconsistent at approximating their continuous counterpartes. Additionally, they will depend on the sequences of shocks that the agents receive. The time-dependence will cause fluctuations in $\hat{\bar{\textbf{M}}}_t$ and $\hat{\bar{\textbf{C}}}_t$. Therefore an informal way to measure the precision of our approximations is to examine the amplitude of these fluctuations:
+# If we do not use enough agents, our distributions of agents over state variables will be inconsistent at approximating their continuous counterpartes. Additionally, they will depend on the sequences of shocks that the agents receive. The time-dependence will cause fluctuations in $\aggMest_t$ and $\aggCest_t$. Therefore an informal way to measure the precision of our approximations is to examine the amplitude of these fluctuations:
 #
 # 1. Simulate the economy for a long time $T_0$.
-# 2. Sample our aggregate estimates at regular intervals after $T_0$. Letting the sampling times be $\mathcal{T}\equiv \{T_0 + \Delta t\times n\}_{n=0,1,...,N}$, obtain $\{\hat{\bar{\textbf{M}}}_t\}_{t\in\mathcal{T}}$ and $\{\hat{\bar{\textbf{C}}}_t\}_{t\in\mathcal{T}}$.
-# 3. Compute the variance of approximation samples $\text{Var}\left(\{\hat{\bar{\textbf{M}}}_t\}_{t\in\mathcal{T}}\right)$ and $\text{Var}\left(\{\hat{\bar{\textbf{C}}}_t\}_{t\in\mathcal{T}}\right)$.
+# 2. Sample our aggregate estimates at regular intervals after $T_0$. Letting the sampling times be $\mathcal{T}\equiv \{T_0 + \Delta t\times n\}_{n=0,1,...,N}$, obtain $\{\aggMest_t\}_{t\in\mathcal{T}}$ and $\{\aggCest_t\}_{t\in\mathcal{T}}$.
+# 3. Compute the variance of approximation samples $\text{Var}\left(\{\aggMest_t\}_{t\in\mathcal{T}}\right)$ and $\text{Var}\left(\{\aggCest_t\}_{t\in\mathcal{T}}\right)$.
 #
 # We will now perform exactly this experiment. We will examine the fluctuations in aggregates when they are approximated using the basic simulation strategy and Harmenberg's permanent-income-neutral measure. Since each approximation can be made arbitrarily good by increasing the number of agents it uses, we will examine the variances of aggregates for various sample sizes.
 #
@@ -305,7 +314,7 @@ axs[1].grid()
 plt.show()
 
 # %% [markdown]
-# The previous plot highlights the gain in efficiency from Harmenberg's method: it attains any given level of precission ($\text{Var}\left(\{\hat{\bar{\textbf{M}}}_t\}_{t\in\mathcal{T}}\right)$) with roughly **one tenth** of the agents needed by the standard method to achieve that same level.
+# The previous plot highlights the gain in efficiency from Harmenberg's method: it attains any given level of precission ($\text{Var}\left(\{\aggMest_t\}_{t\in\mathcal{T}}\right)$) with roughly **one tenth** of the agents needed by the standard method to achieve that same level.
 #
 # We now examine consumption.
 
